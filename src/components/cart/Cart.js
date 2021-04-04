ab98267fbc6e96da856e54d7d06dced56d0c05fa
arttusulkonen <s2001148@edu.bc.fi>
2021-04-04 19:09:35 +0300

    add cart component, and cart icon

---
+++ src/components/cart/Cart.js
@@ -0,0 +1,1 @@
+import React from 'react';const Cart = (props) => {  const { quantity = 0 } = props;  return (    <div className='cart blue darken-4 white-text'>      <i className="material-icons">shopping_cart</i>      {quantity ? <span className='cart-quantity'>{quantity}</span> : null}    </div>  );};export default Cart;
