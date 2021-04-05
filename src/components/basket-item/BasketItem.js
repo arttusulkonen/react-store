99695585c5a97c04f7bb97022f20119ec732719a
arttusulkonen <s2001148@edu.bc.fi>
2021-04-05 12:34:13 +0300

    show in cart items and calc price

--- src/components/basket-item/BasketItem.js
+++ src/components/basket-item/BasketItem.js
@@ -1,1 +1,1 @@
-import React from 'react';const BasketItem = () => {  return (    <div>    </div>  );};export default BasketItem;
+import React from 'react';const BasketItem = (props) => {  const {    id,    name,    price,    quantity  } = props;  return (    <li className="collection-item">{name} x{quantity} = {price}      <span className="secondary-content"><i className="material-icons basket-delete">close</i></span>    </li>  );};export default BasketItem;
